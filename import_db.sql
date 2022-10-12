109c63de866d4ac48bfb8492f2114d9464721327
Ethan Graham <ethangraham43@gmail.com>
2022-10-12 12:15:59 -0400

    finished sql portion?

--- import_db.sql
+++ import_db.sql
@@ -24,3 +24,27 @@ CREATE TABLE question_follows (
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY NOT NULL,
+    body TEXT,
+    question_id INTEGER NOT NULL,
+    reply_id INTEGER,
+    user_id INTEGER NOT NULL,
+
+
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+    FOREIGN KEY (reply_id) REFERENCES replies(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_likes(
+    id INTEGER PRIMARY KEY NOT NULL,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+)
+
+
+
